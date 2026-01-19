cdf0e10fedfc9fac7615b21b87ab33e0c08d6e5b
TuanNguyenDUT <futherr123@gmail.com>
2026-01-19 12:55:09 +0700

    Model auction participation with composite key and deposit relation

--- Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/models/ThamGiaDauGia.java
+++ Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/models/ThamGiaDauGia.java
@@ -1,12 +1,15 @@
 package io.github.guennhatking.libra_auction.models;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,11 +26,16 @@ public class ThamGiaDauGia {
     @MapsId("phienDauGiaId")
     @JoinColumn(name = "phien_dau_gia_id")
     private PhienDauGia phienDauGia;
+    
     @ManyToOne
     @MapsId("nguoiThamGiaId")
     @JoinColumn(name = "nguoi_tham_gia_id")
     private NguoiDung nguoiThamGia;
 
+    @Column(name = "thoi_gian_dang_ky")
     private LocalDateTime thoiGianDangKy;
+
+    @OneToMany(mappedBy = "thamGiaDauGia")
+    private List<TienCoc> dsTienCoc;
 }
 
