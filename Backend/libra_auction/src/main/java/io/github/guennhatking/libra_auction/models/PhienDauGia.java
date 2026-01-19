9ff049e877c37bea5146aca5291fc2fcc1c24ab9
TuanNguyenDUT <futherr123@gmail.com>
2026-01-19 12:54:53 +0700

    Update auction session entity with bid and participation relations

--- Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/models/PhienDauGia.java
+++ Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/models/PhienDauGia.java
@@ -2,12 +2,14 @@ package io.github.guennhatking.libra_auction.models;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -29,9 +31,12 @@ public class PhienDauGia {
     @Column(name = "trang_thai_phien")
     private String trangThaiPhien;
 
-    @ManyToMany(mappedBy = "phienDaThamGia")
-    private Set<NguoiDung> nguoiThamGia = new HashSet<>();
+    @OneToMany(mappedBy = "phienDauGia")
+    private List<ThamGiaDauGia> dsThamGiaDauGia;
 
+    @OneToMany(mappedBy = "phienDauGia")
+    private List<BanGhiPhienDauGia> dsBanGhiDauGia;
+    
     @OneToOne(mappedBy = "phienDauGia")
     private ThongTinPhienDauGia thongTin;
 }
