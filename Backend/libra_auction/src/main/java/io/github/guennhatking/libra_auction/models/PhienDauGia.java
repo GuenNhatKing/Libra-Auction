8f84ee379592ff88e142063e51f420dc98626f14
TuanNguyenDUT <futherr123@gmail.com>
2026-01-19 12:00:07 +0700

    Add inverse relationships for participants and auction details in PhienDauGia

--- Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/models/PhienDauGia.java
+++ Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/models/PhienDauGia.java
@@ -1,10 +1,14 @@
 package io.github.guennhatking.libra_auction.models;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,10 +21,17 @@ public class PhienDauGia {
     @Id
     @Column(length = 50)
     private String id;
-
+    @Column(name = "thoi_gian_bat_dau")
     private LocalDateTime thoiGianBatDau;
+    @Column(name = "thoi_gian_ket_thuc")
     private LocalDateTime thoiGianKetThuc;
 
     @Column(name = "trang_thai_phien")
     private String trangThaiPhien;
+
+    @ManyToMany(mappedBy = "phienDaThamGia")
+    private Set<NguoiDung> nguoiThamGia = new HashSet<>();
+
+    @OneToOne(mappedBy = "phienDauGia")
+    private ThongTinPhienDauGia thongTin;
 }
