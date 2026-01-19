9731ea9c2b5a540f3598ad4d76a064ed1aba29f2
TuanNguyenDUT <futherr123@gmail.com>
2026-01-19 11:56:18 +0700

    Add and complete JPA relationships between NguoiDung and auction sessions

--- Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/models/NguoiDung.java
+++ Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/models/NguoiDung.java
@@ -1,9 +1,17 @@
 package io.github.guennhatking.libra_auction.models;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -27,4 +35,14 @@ public class NguoiDung {
 
     private String sdt;
     private String cccd;
+
+    @ManyToMany
+    @JoinTable(
+        name = "tham_gia_dau_gia",
+        joinColumns = @JoinColumn(name = "nguoi_tham_gia_id"),
+        inverseJoinColumns = @JoinColumn(name = "phien_dau_gia_id")
+    )
+    private Set<PhienDauGia> phienDaThamGia = new HashSet<>();
+    @OneToMany(mappedBy = "chuTaiSan")
+    private List<ThongTinPhienDauGia> phienDaTao = new ArrayList<>();
 }
