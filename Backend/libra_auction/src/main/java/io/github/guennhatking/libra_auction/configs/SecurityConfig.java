22948b0c96c71e61f486797d40bb738a93276cb4
GuenNhatKing <guennhatking@gmail.com>
2026-04-29 08:56:38 +0700

    fix(backend): :bug: change auth api not match

--- Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/configs/SecurityConfig.java
+++ Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/configs/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/public/**").permitAll()
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
