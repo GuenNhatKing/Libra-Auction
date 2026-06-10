7d7ffe299c4511b2eb38463f06c656848b23d1c3
GuenNhatKing <guennhatking@gmail.com>
2026-06-10 13:31:40 +0700

    fix banner

--- Frontend/libra_auction/components/main/home/home_banner.tsx
+++ Frontend/libra_auction/components/main/home/home_banner.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 export default function HomeBanner() {
     return (
-        <div className='relative w-full h-180'>
+        <div className='relative w-full min-h-180'>
             <div className='absolute size-full'>
                 <Image src={home_banner} alt="" className='size-full object-cover'></Image>
             </div>
             <div className='absolute size-full grid grid-cols-2'>
                 <div className='col-start-2 px-8 py-18'>
-                    <div className="w-full h-[76vh] flex flex-col items-start bg-(--background-color)/90 rounded-2xl px-9 py-12">
+                    <div className="w-full min-h-[76vh] flex flex-col items-start bg-(--background-color)/90 rounded-2xl px-9 py-12">
                         <p className="font-semibold tracking-widest">Live Auctions Now Open</p>
                         <h1 className="font-extrabold text-[clamp(2.5rem,var(--home-banner-title-font-size),6rem)] text-(--primary-color)">
                             Discover and Join Live Online Auctions
